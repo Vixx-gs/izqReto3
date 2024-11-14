257dad958ced7946b37cc72e57c8d56a7eb0025e
Vixx-gs <victorgallegoserrate@gmail.com>
2024-11-14 08:37:47 +0100

    Funcion para hacer fecha dar fecha hecha

--- izqReto3/src/Ejercicios/MainReto3.java
+++ izqReto3/src/Ejercicios/MainReto3.java
@@ -1,9 +1,32 @@
 package Ejercicios;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
 public class MainReto3 {
 
 	public static void main(String[] args) {
 		
 	}
 
+	
+	//funcion para decir fecha
+	public static LocalDate dimeFecha(String fechafrase, Scanner sc) {
+		DateTimeFormatter formato1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		do
+		{
+			try
+			{
+				System.out.println(fechafrase);
+				String s = sc.nextLine();
+				LocalDate fecha = LocalDate.parse(s, formato1);
+				return fecha;
+			}
+			catch(Exception ex)
+			{
+				System.out.println("Introduce una fecha correcta");
+			}
+		}while(true);
+	}
 }
